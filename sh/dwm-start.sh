02626068595988ae97b39b123e7a9044c620e9f0
Ne-gh <printfne@gmail.com>
2022-11-16 04:09:00 +0800

    init

---
+++ sh/dwm-start.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+# 关闭笔记本自带键盘
+xinput disable "AT Translated Set 2 keyboard"
+xinput disable "UNIW0001:00 093A:1336 Touchpad"
+while true
+do
+    bash ./dwm-upstart.sh
+    ./Up.out
+    sleep 0.05s
+done
