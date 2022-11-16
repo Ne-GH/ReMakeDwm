721b894a776282d2d5030f509266c865f3f86093
Ne-gh <printfne@gmail.com>
2022-11-16 17:21:38 +0800

    update bash in sh directory

--- sh/dwm-start.sh
+++ sh/dwm-start.sh
@@ -3,9 +3,10 @@
 # 关闭笔记本自带键盘
 xinput disable "AT Translated Set 2 keyboard"
 xinput disable "UNIW0001:00 093A:1336 Touchpad"
+
 while true
 do
-    bash ./dwm-upstart.sh
-    ./Up.out
-    sleep 0.05s
+    bash ~/.dwm/sh/dwm-upstart.sh
+#     ./Up.out
+    sleep 0.1s
 done
